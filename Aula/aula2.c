2335d1eb350e7768b82110a37f638dc1776daeb4
FernandoGrimello <112512315+fernandogrimello@users.noreply.github.com>
2025-06-05 17:08:31 -0300

    Add files via upload

---
+++ Aula/aula2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int idade = 37;
+    float altura = 1.78;
+    char letra= 'A';
+
+    printf("Idade: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+    printf("Letra: %c\n", letra);
+
+    return 0;
+}
