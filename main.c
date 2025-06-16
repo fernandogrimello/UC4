fb1e493bd3253d9e450aeb4b155f15de127d7060
FernandoGrimello <112512315+fernandogrimello@users.noreply.github.com>
2025-06-16 15:49:06 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+/* tipos de dados primitivos
+
+int	Números inteiros	int idade = 25;
+float	Números reais com menos precisão	float peso = 70.5;
+double	Números reais com mais precisão	double pi = 3.141592;
+char	Um único caractere	char letra = 'A';
+
+Variaveis
+São nomes simbólicos para armazenar dados em memória. Ex:
+
+int idade = 30;
+char letra = 'B';
+
+Arrays (vetores):
+Estruturas que armazenam vários valores do mesmo tipo, acessíveis por índice (iniciando do 0). Ex:
+
+int numeros[5] = {10, 20, 30, 40, 50};
+printf("%d", numeros[2]);  // imprime 30
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    /* code */
+    return 0;
+}
